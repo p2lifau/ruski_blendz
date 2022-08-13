43c275231e05f731f877b4d80f09f09891790323
p2lifau <p2lifau@gmail.com>
2022-08-12 18:54:27 -0700

    navbar-restructure responsive design

---
+++ app.js
@@ -0,0 +1,6 @@
+// Hamburger Menu selectors
+const toggleButton = document.getElementsByClassName('toggle-button')[0];
+const navbarLinks = document.getElementsByClassName('navbar-links')[0];
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active');
+});
